073f311ba45c2902ca278fff0b570bf4bebf7265
rivenshell <rivenshell111@gmail.com>
2024-05-05 17:25:39 -0400

    project page update

--- app/components/Portfolio.tsx
+++ app/components/Portfolio.tsx
@@ -9,7 +9,7 @@ const Portfolio = () => {
           aria-label="Projects Page"
           className=" flex justify-center items-center text-4xl pt-4"
         >
-          Projects test
+          entry
         </h1>
       </div>
     </>
